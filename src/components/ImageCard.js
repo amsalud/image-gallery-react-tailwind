aea1b8dc141b7fa81f4ae74126b8f848e61000f5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-12 21:47:02 -0400

    Render image prop data

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,38 +1,45 @@
 import React from 'react'
 
-export const ImageCard = () => {
+export const ImageCard = ({image}) => {
+    const {
+        user,
+        views,
+        downloads,
+        likes,
+        tags,
+        webformatURL
+    } = image;
+
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+        <img src={webformatURL} alt={tags} className="w-full"/>
         <div className="px-6 py-4">
           <div className="font-bold text-purple-500 text-xl mb-2">
-            Photo by John Doe
+            Photo by {user}
           </div>
           <ul>
             <li>
               <strong>Views:</strong>
-              4000
+              {views}
             </li>
             <li>
               <strong>Downloads:</strong>
-              50
+              {downloads}
             </li>
             <li>
               <strong>Likes:</strong>
-              300
+              {likes}
             </li>
           </ul>
         </div>
         <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag1
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            #tag3
-          </span>
+            {tags.split(', ').map((tag, index) =>{
+                return (
+                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                    {tag}
+                    </span>
+                )
+            })}
         </div>
       </div>
     )
