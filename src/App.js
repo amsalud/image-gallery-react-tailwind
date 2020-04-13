41bc6c8f93c7e1aca478d2e84713cda14114f591
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-12 21:28:27 -0400

    Fetch images from pixabay

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
-  
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res =>res.json())
+    .then(data =>{ 
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err))
+  }, [term]);
+
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
