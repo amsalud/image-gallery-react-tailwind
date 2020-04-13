14ca2b78fe46a7a973fe04c18099cd5428cc19ef
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-12 22:19:22 -0400

    Add no results found placeholder

--- src/App.js
+++ src/App.js
@@ -20,11 +20,15 @@ function App() {
   return (
     <div className="container mx-auto">
       <ImageSearch setSearchTerm={(text)=> setTerm(text)}/>
+      {!isLoading && images.length === 0 &&
+        <h1 className="text-6xl text-center mx-auto mt-32">No Results found</h1> 
+      }
       { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading Please wait...</h1> :
         <div className="grid grid-cols-3 gap-4">
           {images.map(image=> (<ImageCard key={image.id} image={image}/>))}
         </div>
       }
+    
     </div>
   );
 }
