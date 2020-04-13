cd577d82f4888ccdd75f1173d95776aa65961ff4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-12 22:04:24 -0400

    Create ImageSearch.js

---
+++ src/components/ImageSearch.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+export const ImageSearch = () => {
+    const [text, setText] = useState('');
+    return (
+        <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+            <form className="w-full max-w-sm" action="">
+                <div className="flex item-center border-b border-b-2 border-teal-500 py-2">
+                    <input type="text" value={text} onChange={e=> setText(e.target.value)} placeholder="Search for Images" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" />
+                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">Search</button>
+                </div>
+            </form>
+        </div>
+    )
+}
